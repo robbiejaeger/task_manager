3ccb8a849851d719ece688b96b2f66eedfa1e0ff
Robbie Jaeger <jaeger.rob@gmail.com>
2016-05-05 11:52:37 -0600

    Add task routes

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,19 @@ class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/' do
-    'hello, world!'
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ["task1", "task2","task3"]
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
   end
 end
