b1f02c3235788553a80239c5530e25d43346b645
Robbie Jaeger <jaeger.rob@gmail.com>
2016-05-09 18:00:37 -0600

    Completed tutorial

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,3 +1,5 @@
+require 'models/task_manager'
+
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
@@ -6,7 +8,7 @@ class TaskManagerApp < Sinatra::Base
   end
 
   get '/tasks' do
-    @tasks = ["task1", "task2","task3"]
+    @tasks = task_manager.all
     erb :index
   end
 
@@ -15,6 +17,17 @@ class TaskManagerApp < Sinatra::Base
   end
 
   post '/tasks' do
-    "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+    task_manager.create(params[:task])
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id' do |id|
+    @task = task_manager.find(id.to_i)
+    erb :show
+  end
+
+  def task_manager
+    database = YAML::Store.new('db/task_manager')
+    @task_manager ||= TaskManager.new(database)
   end
 end
