dc946471c0bddce0a5996ebc60612a8c0d571310
Robbie Jaeger <jaeger.rob@gmail.com>
2016-05-12 15:07:26 -0600

    Migrate to sequel and postgres database

--- test/models/task_manager_test.rb
+++ test/models/task_manager_test.rb
@@ -4,13 +4,13 @@ class TaskManagerTest < Minitest::Test
   include TestHelpers
 
   def test_it_creates_a_task
-    task_manager.create({title: "TDD", description: "Learn to Test"})
+    task_id = task_manager.create({title: "TDD", description: "Learn to Test"})
 
-    task = task_manager.find(1)
+    task = task_manager.find(task_id)
 
     assert_equal "TDD", task.title
     assert_equal "Learn to Test", task.description
-    assert_equal 1, task.id
+    assert_equal task_id, task.id
   end
 
   def test_can_get_all_the_tasks
@@ -27,26 +27,26 @@ class TaskManagerTest < Minitest::Test
 
   def test_can_find_a_task
     task_manager.create({title: "Task1", description: "Description 1"})
-    task_manager.create({title: "Task2", description: "Description 2"})
+    task_id = task_manager.create({title: "Task2", description: "Description 2"})
 
-    task = task_manager.find(2)
+    task = task_manager.find(task_id)
 
     assert_equal "Task2", task.title
   end
 
   def test_can_update_a_task
-    task_manager.create({title: "Task1", description: "Description 1"})
+    task_id = task_manager.create({title: "Task1", description: "Description 1"})
 
-    task_manager.update(1, {title: "Task1 Updated", description: "Description 1"})
-    task = task_manager.find(1)
+    task_manager.update(task_id, {title: "Task1 Updated", description: "Description 1"})
+    task = task_manager.find(task_id)
 
     assert_equal "Task1 Updated", task.title
   end
 
   def test_can_destroy_a_task
-    task_manager.create({title: "Task1", description: "Description 1"})
+    task_id = task_manager.create({title: "Task1", description: "Description 1"})
 
-    task_manager.destroy(1)
+    task_manager.destroy(task_id)
     tasks = task_manager.all
 
     assert_equal [], tasks
